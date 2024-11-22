76719e3ef1a972a413b2f50d55373654e7a303ea
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-11-22 18:09:48 +0300

    add g

--- t_b_victorina.py
+++ t_b_victorina.py
@@ -1,62 +1,79 @@
-import random
-from telebot import TeleBot
+import telebot
 
-# Токен вашего бота
+# Вставьте сюда ваш токен от BotFather
 TOKEN = '7247198623:AAETbxbA8OhoUAvVPziaHUAJJpa5TWMRm9Y'
 
-# Создаем экземпляр бота
-bot = TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN)
 
-# Вопросы и ответы для разных уровней сложности
-easy_questions = [{'question1': 'Какой город является столицей Франции?','answer1': 'Париж'}, {'question2': 'Самая длинная река в мире?','answer2': 'Амазонка',}]
+# Список вопросов и ответов для каждого уровня сложности
+questions = {
+    'easy': [
+        {'question': 'Какой город является столицей Франции?', 'answer': 'Париж'},
+        {'question': 'Какое самое большое озеро в мире?', 'answer': 'Каспийское море'},
+        {'question': 'В каком городе находится Эйфелева башня?', 'answer': 'Париже'}
+    ],
+    'normal': [
+        {'question': 'Где находится гора Эверест?', 'answer': 'Непал'},
+        {'question': 'Столица Японии?', 'answer': 'Токио'},
+        {'question': 'Самое глубокое озеро в мире?', 'answer': 'Байкал'}
+    ],
+    'hard': [
+        {'question': 'Самая длинная река в мире?', 'answer': 'Нил'},
+        {'question': 'Самый большой океан в мире?', 'answer': 'Тихий океан'},
+        {'question': 'Страна с самой длинной береговой линией?', 'answer': 'Канада'}
+    ]
+}
 
-medium_questions = [{'question3': 'Самая высокая гора на земле?','answer3': 'Эверест'}, {'question4': 'Какой океан самый большой в мире?','answer4': 'Тихий океан'}]
 
-hard_questions = [ { 'question5': 'Какой самый маленький континент в мире?', 'answer5': 'Австралия' },{'question6': 'Какая пустыня самая большая?', 'answer6': 'Сахара'}]
-
-
-# Функция для отправки сообщения о старте игры
 @bot.message_handler(commands=['start'])
-def start_game(message):
-    bot.send_message(
-        message.chat.id,
-        'Добро пожаловать в географическую викторину!\n\nВыберите уровень сложности:\n/level_easy - Легкий\n/level_medium - Средний\n/level_hard - Сложный'
-    )
+def send_welcome(message):
+    bot.reply_to(message,
+                 f'Привет! Хочешь сыграть в викторину? Выбери уровень сложности:\n/level_easy\n/level_normal\n/level_hard')
 
 
-# Функция для обработки команды /level_easy
-@bot.message_handler(commands=['level_easy'])
-def level_easy(message):
-    ask_question(easy_questions, message.chat.id)
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if message.text == '/level_easy':
+        start_quiz('easy', message.chat.id)
+    elif message.text == '/level_normal':
+        start_quiz('normal', message.chat.id)
+    elif message.text == '/level_hard':
+        start_quiz('hard', message.chat.id)
 
 
-# Функция для обработки команды /level_medium
-@bot.message_handler(commands=['level_medium'])
-def level_medium(message):
-    ask_question(medium_questions, message.chat.id)
+def start_quiz(level, chat_id):
+    score = 0
+    current_question_index = 0
+    total_questions = len(questions[level])
 
-
-# Функция для обработки команды /level_hard
-@bot.message_handler(commands=['level_hard'])
-def level_hard(message):
-    ask_question(hard_questions, message.chat.id)
-
-
-# Функция для генерации вопроса и проверки ответа
-def ask_question(questions, chat_id):
-    question = random.choice(questions)
-    bot.send_message(chat_id, f'Вопрос: {question["question"]}')
-
-    @bot.message_handler(func=lambda msg: True)
-    def check_answer(message):
-        if message.text.lower() == question['answer'].lower():
-            bot.send_message(chat_id, 'Правильно!')
-            ask_question(questions, chat_id)
+    def ask_next_question():
+        nonlocal current_question_index
+        if current_question_index < total_questions:
+            question = questions[level][current_question_index]['question']
+            bot.send_message(chat_id, question)
+            bot.register_next_step_handler_by_chat_id(chat_id, check_answer)
         else:
-            bot.send_message(chat_id, f'Неправильно. Правильный ответ: {question["answer"]}.')
-            ask_question(questions, chat_id)
+            finish_quiz(score, total_questions, chat_id)
+
+    def check_answer(user_message):
+        nonlocal current_question_index, score
+        answer = questions[level][current_question_index]['answer'].lower().strip()
+        user_answer = user_message.text.lower().strip()
+        if user_answer == answer:
+            score += 1
+            bot.send_message(chat_id, 'Правильно!')
+        else:
+            bot.send_message(chat_id, f'Не правильно. Правильный ответ: {answer}')
+
+        current_question_index += 1
+        ask_next_question()
+
+    def finish_quiz(score, total_questions, chat_id):
+        bot.send_message(chat_id, f"Твой результат: {score}/{total_questions}")
+
+    # Начинаем викторину
+    ask_next_question()
 
 
 if __name__ == '__main__':
-    # Запускаем бесконечный цикл для работы бота
-    bot.polling(none_stop=True)
+    bot.polling()
